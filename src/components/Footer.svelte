b4e149db39fc557048b2c222eb1fba4e618d4f05
joemmalatesta <joemmalatesta@gmail.com>
2023-05-09 23:20:51 -0400

    try again?

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,4 +4,4 @@
 <footer class="flex justify-between my-7">
     <!-- Replace with Logo Maybe -->
     <a class="text-3xl" href="https://joemmalatesta.com"><img src="logo.svg" class="w-16" alt=""></a>
-</footer>
+</footer> 
