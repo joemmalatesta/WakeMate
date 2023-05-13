a8d44eb0f82ae03761435890da1801e3ecc731bd
joemmalatesta <joemmalatesta@gmail.com>
2023-05-12 22:44:34 -0400

    update wording

--- src/components/Benefits.svelte
+++ src/components/Benefits.svelte
@@ -12,7 +12,7 @@
         <div class="md:h-1/3 w-1/2">
             <img class="2xl:w-20 w-16 object-cover"  src="sunrise-icon.svg" alt="Message Icon" />
         </div>
-        <p class="p-2 text-lg md:text-xl 2xl:text-2xl h-2/3">Be in control of your morning, specifying when and how you wake up</p>
+        <p class="p-2 text-lg md:text-xl 2xl:text-2xl h-2/3">Be in control of your morning, specifying the time and days you wake up.</p>
     </li>
     <li class="md:h-64 w-80 flex items-center md:items-start md:flex-col mb-4">
         <div class="md:h-1/3 w-1/2">
